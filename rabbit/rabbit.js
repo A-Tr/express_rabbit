8657b481d280a2aa50edea0aa8b2ecc00a4820c4
A-Tr <alvarotrancon@gmail.com>
2019-02-01 00:14:13 +0100

    Implement async/await and try catch for consumer and producer. implement trace-id in consumer

--- rabbit/rabbit.js
+++ rabbit/rabbit.js
@@ -1,21 +1,24 @@
 const open = require('amqplib').connect('amqp://localhost:5672')
 const logger = require('../logger/logger')
-
-module.exports = () => {
-  open
-    .then((conn) => conn.createChannel())
-    .then(ch => ch.assertQueue('go-to-node')
-      .then(function(ok) {
-        logger.info(`Channel assertion: ${JSON.stringify(ok)}`, {traceId: '1'})
-        return ch.consume('go-to-node', msg => {
-          if (msg !== null) {
-            logger.info(`Message: ${JSON.stringify(msg.content.toString())}`, {traceId: '1'})
-            ch.ack(msg)
-          }
-        })
-      })
-    ).catch(e => {
-      logger.error(`Error with the consumer ${e}`, '1')
+const uuid = require('uuid/v4')
+module.exports = async () => {
+	const traceId = uuid()
+  try {
+    const conn = await open
+    const ch = await conn.createChannel()
+	
+    const ok = await ch.assertQueue('go-to-node')
+    logger.info(`Channel assertion: ${JSON.stringify(ok)}`, {traceId})
+	
+    return ch.consume('go-to-node', msg => {
+      if (msg !== null) {
+        logger.info(`Message: ${JSON.stringify(msg.content.toString())}`, {traceId})
+        ch.ack(msg)
+      }
     })
+  } catch (error) {
+    logger.error(`Error with the consumer ${error}`, {traceId})
+		
+  }
 }
 
